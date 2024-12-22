f7b032a61b08f2233829cbc2ba50bf6871306b25
Gojo-9999 <sasukeuchiha448104@gmail.com>
2024-12-22 13:06:35 +0300

    Add files via upload

---
+++ Personal Portfolio/js/main.js
@@ -0,0 +1,7 @@
+let openMenu = document.querySelector('.ri-menu-5-line')
+let menu = document.querySelector('.burger-menu')
+
+openMenu.addEventListener('click', () => {
+    menu.classList.toggle('menu')
+})
+
